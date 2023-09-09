91452fae02db2fffc6747af60ea8ce13a5c82f48
Nathanv2 <lnathan718@gmail.com>
2023-09-08 21:20:56 -0700

    Update

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Set the speed of the vehicle
+    private float speed = 15.0f;
+    private float turnSpeed = 100.0f;
+
+    // Declares the input variables
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get Inputs to be able to manually control the vehicle
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Allows you to move the vehicle forward with speed
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    }
+}
